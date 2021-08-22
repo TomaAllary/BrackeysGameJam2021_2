023072c4be2ccbf50e98ea50892488dc0318cfeb
allaryt <toma.allary@uqtr.ca>
2021-08-22 17:33:11 -0400

    Added packages and tested tilemaps

---
+++ BrackeysGameJam2021_2/Assets/Test 3D Tilemap/PlacingObject.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlacingObject : MonoBehaviour
+{
+    [SerializeField] private GameObject wallPrefab;
+
+    [SerializeField] private Tilemap tileMap;
+    [SerializeField] private List<Vector3> availablePlaces;
+
+    void Start() {
+        FindLocationsOfTiles();
+    }
+
+    private void FindLocationsOfTiles() {
+        availablePlaces = new List<Vector3>(); // create a new list of vectors by doing...
+
+        for (int n = tileMap.cellBounds.xMin; n < tileMap.cellBounds.xMax; n++) // scan from left to right for tiles
+        {
+            for (int p = tileMap.cellBounds.yMin; p < tileMap.cellBounds.yMax; p++) // scan from bottom to top for tiles
+            {
+                Vector3Int localPlace = new Vector3Int(n, p, (int)tileMap.transform.position.y); // if you find a tile, record its position on the tile map grid
+                
+                if (tileMap.HasTile(localPlace)) {
+                    //Tile at "place"
+                    availablePlaces.Add(localPlace);
+                }
+                else {
+                    //No tile at "place"
+                }
+            }
+        }
+    }
+
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Camera cam = Camera.main;
+
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, LayerMask.GetMask("Terrain"))) {
+
+                Vector3 destination = new Vector3(hit.point.x, tileMap.transform.position.y, hit.point.z);
+
+                Vector3Int gridPos = tileMap.WorldToCell(destination);
+
+                SpawnWall(gridPos);
+
+            }
+
+        }
+    }
+
+    private void SpawnWall(Vector3Int pos) {
+
+        if (availablePlaces.Contains(pos))
+            return;
+
+        Vector3 realPos = tileMap.CellToWorld(pos);
+
+        Instantiate(wallPrefab, realPos, Quaternion.identity);
+    }
+}
---
+++ BrackeysGameJam2021_2/Assets/Test 3D Tilemap/RTSCamera.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTSCamera : MonoBehaviour
+{
+    [SerializeField] float camSpeed = 8.0f;
+    [SerializeField] float rotateCamSpeedH = 25.0f;
+    [SerializeField] float rotateCamSpeedV = 35.0f;
+
+    private Camera cam;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+
+        yaw = cam.transform.eulerAngles.y;
+        pitch = cam.transform.eulerAngles.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float sideSpeed = Input.GetAxis("Horizontal") * camSpeed * Time.deltaTime;
+        cam.transform.position += cam.transform.right * sideSpeed;
+
+        float frontSpeed = Input.GetAxis("Vertical") * camSpeed * Time.deltaTime;
+        Vector3 frontVector = new Vector3(cam.transform.forward.x, 0, cam.transform.forward.z).normalized;
+
+        cam.transform.position += frontVector * frontSpeed;
+
+
+        if (Input.GetKey(KeyCode.LeftAlt)) {
+
+            Cursor.lockState = CursorLockMode.Locked;
+
+            yaw += rotateCamSpeedH * Input.GetAxis("Mouse X") * Time.deltaTime;
+            pitch -= rotateCamSpeedV * Input.GetAxis("Mouse Y") * Time.deltaTime;
+
+            cam.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        }
+        else {
+            Cursor.lockState = CursorLockMode.None;
+
+        }
+
+    }
+}
