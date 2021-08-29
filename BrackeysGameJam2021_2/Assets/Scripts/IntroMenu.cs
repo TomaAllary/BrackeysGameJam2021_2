099e3828e1bcb2a83dac8f70de53ac8607f374c7
matteabe <ben.matteau@gmail.com>
2021-08-28 22:00:52 -0400

    early Intro and story
    Started Intro scene and added story

---
+++ BrackeysGameJam2021_2/Assets/Scripts/IntroMenu.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroMenu : MonoBehaviour
+{
+    public AudioSource mainAudio;
+    public AudioSource storyAudio;
+    bool storyOn;
+    public AudioClip audioClipStory;
+    // Start is called before the first frame update
+    void Start()
+    {
+        storyOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playStory()
+    {
+        if (!storyOn)
+        {
+            mainAudio.volume = .1f;
+            storyAudio.PlayOneShot(audioClipStory);
+            storyOn = true;
+            StartCoroutine(WaitForStory());
+        }
+        else
+        {
+            storyAudio.Stop();
+            mainAudio.volume = 1f;
+            storyOn = false;
+        }
+    }
+    IEnumerator WaitForStory()
+    {
+        yield return new WaitUntil(() => storyAudio.isPlaying == false);
+        mainAudio.volume = 1.0f;
+        storyOn = false;
+    }
+}
